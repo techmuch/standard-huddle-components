92c5429cfeb8c4e8c4a774106a5ea71e953336b5
yann.charront <ycharront3@gatech.edu>
2015-07-24 16:40:18 -0400

    Bar chart and stacked bar chart modif (vertical labeling)

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -6,15 +6,17 @@ define(['jquery', 'knockout', 'd3', 'text!./bar-chart.html'], function($, ko, d3
 		self.element = componentInfo.element;
 		self.firstRender = ko.observable(true);
 		self.data = params.data || ko.observable(null);
-		//self.color = params.color;
-		// test with settings table in db
-		self.color = JSON.parse(params.color());
+		self.color = params.color;
+		// // test with settings table in db
+		// self.color = JSON.parse(params.color());
 		// list variable common to both render() and update()
 		self.x = null;
 		self.y = null;
 		self.xAxis = null;
 		self.yAxis = null;
+		self.yAxis_name = params.yAxis;
 		self.svg = null;
+		
 
 
 		var margin = {top: 20, right: 20, bottom: 30, left: 65};
@@ -27,7 +29,7 @@ define(['jquery', 'knockout', 'd3', 'text!./bar-chart.html'], function($, ko, d3
 		self.render = function() {
 			var data = self.data();
 			//var color = d3.scale.ordinal().range(self.color()[6]);
-			var color = d3.scale.ordinal().range(self.color);
+			var color = d3.scale.ordinal().range(self.color()[6]);
 			//console.log('test render'); // test
 
 			self.x = d3.scale.ordinal().domain(data.map(function(d) { return d.name; })).rangeRoundBands([0, self.width], .1);
@@ -52,7 +54,14 @@ define(['jquery', 'knockout', 'd3', 'text!./bar-chart.html'], function($, ko, d3
 			  self.svg.append("g")
 				  .attr("class", "x axis")
 				  .attr("transform", "translate(0," + self.height + ")")
-				  .call(self.xAxis);
+				  .call(self.xAxis)
+				  .selectAll("text")  
+					.style("text-anchor", "end")
+					.attr("dx", "-.8em")
+					.attr("dy", ".15em")
+					.attr("transform", function(d) {
+						return "rotate(-90)" 
+						});
 
 
 			self.svg.append("g")
@@ -64,7 +73,7 @@ define(['jquery', 'knockout', 'd3', 'text!./bar-chart.html'], function($, ko, d3
 				  .attr("y", -50)
 				  .attr("x", -$(".y.axis")[0].getBBox().height / 2)
 				  .attr("dy", ".71em")
-				  .text("Number of TCs");
+				  .text(self.yAxis_name);
 
 /* 			  self.svg.append("g")
 				  .attr("class", "y axis")
@@ -98,7 +107,7 @@ define(['jquery', 'knockout', 'd3', 'text!./bar-chart.html'], function($, ko, d3
 		self.update = function update() {
 			var data = self.data();
 			//var color = d3.scale.ordinal().range(self.color()[6]);
-			var color = d3.scale.ordinal().range(self.color);
+			var color = d3.scale.ordinal().range(self.color()[6]);
 			//console.log('test update');
 
 			var transDuration = 2500;
