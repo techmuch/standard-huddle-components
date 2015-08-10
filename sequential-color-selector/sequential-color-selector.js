38480be85fcb61b22ac3ce2b6eb97cafb3b3aa77
Christopher Frank <christopher.frank@gatech.edu>
2015-08-10 16:24:59 -0400

    Colors added

--- sequential-color-selector/sequential-color-selector.js
+++ sequential-color-selector/sequential-color-selector.js
@@ -9,7 +9,10 @@ define(['jquery', 'knockout', 'd3', 'text!./sequential-color-selector.html'], fu
 					.attr("class", "seq-palette")
 					.attr("title", function(d) { return d.key; })
 					.on("click", function(d) { 
-						tm.selectedColors(d.value);
+						tm.selectedColorsStackedBarChart(d.value);
+						tm.selectedColorsGroupedBarChart(d.value);
+						tm.selectedColorsPieChart(d.value);
+						tm.selectedColorsBarChart(d.value);
 					})
 					.selectAll(".seq-swatch")
 					.data(function(d) { return d.value[d3.keys(d.value).map(Number).sort(d3.descending)[0]]; })
